8280d95872cb85825d923928a66ff2d8c91e949b
Kevin McCarthy <mccarthyk@hcflgov.net>
2017-11-06 08:33:44 -0500

    set navbar background to white

--- src/components/HcNavigation.vue
+++ src/components/HcNavigation.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav id="hc-nav" class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav id="hc-nav" class="navbar navbar-expand-lg navbar-light bg-white">
     <div class="container-fluid">
 
       <a class="navbar-brand" :href="logoHref">
